f960556ca509a795912bd23a95aff9b8420a3595
CC <CC@users.noreply.local>
2026-03-01 20:22:14 +0530

    Added app.py and updated hello.py

---
+++ week1/app.py
@@ -0,0 +1,6 @@
+print("Your Learning Path: " \
+"\n\t-Python Basics " \
+"\n\t - Data Structures " \
+"\n \t - Object-Oriented Programming " \
+"\n \t - Functional Programming " \
+"\n \t - Web Development with Flask")
